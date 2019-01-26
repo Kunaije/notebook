215413f4bb2c4655f489753127d0f0a6514eb39f
Kunaije <kunaije@kuna.fritz.box>
2019-01-26 22:11:39 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
-</template>
+      <div id="app">
+          <Notebook @change-page="changePage" @new-page="newPage" :pages="pages" :activePage="index" />
+          <Page @save-page="savePage" @delete-page="deletePage" :page="pages[index]" />
+      </div>
+    </template>
 
-<script>
-import HelloWorld from './components/HelloWorld'
+    <script>
+    import Notebook from './components/Notebook'
+    import Page from './components/Page'
+    import Firebase from 'firebase'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    var database = Firebase.initializeApp({
+      apiKey: 'apiKey',
+      authDomain: 'authDomain',
+      databaseURL: 'databaseURL',
+      projectId: 'projectId',
+      storageBucket: 'storageBucket',
+      messagingSenderId: 'messagingSenderId'
+    }).database().ref();
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    export default {
+      name: 'app',
+      components: {
+        Notebook,
+        Page
+      },
+      data: () => ({
+        pages: [],
+        index: 0
+      }),
+      mounted() {
+        database.once('value', (pages) => {
+          pages.forEach((page) => {
+            this.pages.push({
+              ref: page.ref,
+              title: page.child('title').val(),
+              content: page.child('content').val()
+            })
+          })
+        })
+      },
+      methods: {
+        newPage () {
+          this.pages.push({
+            title: '',
+            content: ''
+          })
+          this.index = this.pages.length - 1
+        },
+        changePage (index) {
+          this.index = index
+        },
+        savePage () {
+          var page = this.pages[this.index]
+          if (page.ref) {
+            this.updateExistingPage(page)
+          } else {
+            this.insertNewPage(page)
+          }
+        },
+        updateExistingPage (page) {
+          page.ref.set({
+            title: page.title,
+            content: page.content
+          })
+        },
+        insertNewPage (page) {
+          page.ref = database.push(page)
+        },
+        deletePage () {
+          var ref = this.pages[this.index].ref
+          ref && ref.remove()
+          this.pages.splice(this.index, 1)
+          this.index = Math.max(this.index - 1, 0)
+        }
+      }
+    }
+    </script>
+
+    <style>
+    html, body, #app {
+        height: 100%;
+    }
+
+    body {
+        margin: 0;
+    }
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        display: flex;
+        flex-direction: row;
+    }
+    </style>
